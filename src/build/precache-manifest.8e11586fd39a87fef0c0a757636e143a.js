e79b58ad70eced7bb6883b45f3498e3af7bb27fc
Joey Chen <joeyshchen@gmail.com>
2020-04-16 10:51:54 -0700

    Initial build

---
+++ src/build/precache-manifest.8e11586fd39a87fef0c0a757636e143a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631bc861560bb4db0e49c6db4787b9b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e8a307231cfca9d9217",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9623ba4b66ac9046d370",
+    "url": "/static/js/2.d01be896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d01be896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8a307231cfca9d9217",
+    "url": "/static/js/main.c6a051c3.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
